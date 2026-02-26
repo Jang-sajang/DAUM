febff7bfd4f69e0448825740fd8f037166317c59
LAPTOP-GRF4BCFO\te040 <skehwkddngur@nate.com>
2026-02-27 03:00:04 +0900

    서버 analytics 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,22 @@ import About from './pages/About';
 import Notice from './pages/Notice';
 import NoticeDetail from './pages/NoticeDetail';
 import Contact from './pages/Contact';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Layout />}>
-                <Route index element={<Home />} />
-                <Route path="about" element={<About />} />
-                <Route path="notice" element={<Notice />} />
-                <Route path="notice/:id" element={<NoticeDetail />} />
-                <Route path="contact" element={<Contact />} />
-            </Route>
-        </Routes>
+        <>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="notice" element={<Notice />} />
+                    <Route path="notice/:id" element={<NoticeDetail />} />
+                    <Route path="contact" element={<Contact />} />
+                </Route>
+            </Routes>
+            <Analytics />
+        </>
     );
 }
 
